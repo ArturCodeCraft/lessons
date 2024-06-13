5814622b81604ab0c4070ec961d193f109b22612
Artur <140711980+ArturCodeCraft@users.noreply.github.com>
2024-06-13 11:57:45 +0300

    Add files via upload

---
+++ lesson_22_homework/js/script.js
@@ -0,0 +1,24 @@
+// document.addEventListener('click', function (e) {
+// 	const targetElement = e.target;
+// 	if (targetElement.closest('.menu__icon')) {
+// 		document.documentElement.classList.toggle('menu-open');
+// 	}
+// })
+
+// for scroll TEST
+// window.addEventListener('scroll', function () {
+// 	if (window.scrollY > 0) {
+// 		document.body.classList.add('scroll');
+// 	} else {
+// 		document.body.classList.remove('scroll');
+// 	}
+// });
+
+
+for (let i = 1; i <= 10; i++) {
+	console.log(`Пункт №${i}`);
+}
+
+if (2 * 20 <= 10 || 30 / 2 < 5 && 10 <= "10" || 20 === "20") {
+	console.log('000');
+}
